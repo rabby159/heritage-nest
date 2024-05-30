8c726e2980b828b3c04636233e9ea4ea243579a8
Md Rabby <ablazerabbi159@gmail.com>
2024-05-30 19:17:37 +0600

    home page property 2 complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Property from '../Property/Property';
+import Property2 from '../Property/Property2';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Property></Property>
+            <Property2></Property2>
         </div>
     );
 };
---
+++ src/Pages/Property/Property2.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import frame3 from "../../assets/frame3.jpeg";
+import frame4 from "../../assets/frame4.jpeg";
+import playButton from "../../assets/playbutton.png";
+import { RiFindReplaceLine } from "react-icons/ri";
+
+const Property2 = () => {
+  return (
+    <div className="mx-5 md:mx-20 grid grid-cols-1 xl:grid-cols-2 mb-10">
+      <div>
+        <img
+          className="w-[485px] h-[400px] rounded"
+          src={frame3}
+          alt="frame1 img"
+        />
+        <img
+          className="w-[452px] h-[240px] rounded md:ml-28 -mt-32"
+          src={frame4}
+          alt="frame2 img"
+        />
+        <div className="relative ">
+          <div className="absolute left-4 md:left-96 w-24 -mt-72 ml-16">
+            <img src={playButton} alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="p-10">
+        <div className="flex items-center gap-3">
+          <div>
+            <div className="border-2 w-14 border-orange-400"></div>
+          </div>
+          <div className="text-[#F06711] font-semibold text-lg">
+            <p>Property buying</p>
+          </div>
+        </div>
+        <div>
+          <h2 className="font-bold text-4xl">Efficient and Transparent Home Buying Solutions</h2>
+          <p className="mt-6 mb-12">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+          <button className="bg-[#ECF5FF] p-3 text-blue-600 font-medium rounded-sm inline-flex items-center gap-2"><RiFindReplaceLine /> Find Property</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Property2;
