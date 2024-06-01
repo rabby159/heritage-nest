e7692a43c0ea47e56bbd13c3208198b8b3f8782f
Md Rabby <ablazerabbi159@gmail.com>
2024-06-01 17:36:20 +0600

    location update

--- src/Pages/Seller/Seller.jsx
+++ src/Pages/Seller/Seller.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const Seller = () => {
-    return (
-        <div>
-           
-           
+  return (
+    <div className="mx-5 md:mx-20 my-20">
+      <div className="bg-[#ECF5FF] p-5 rounded-md">
+        <h3 className="text-xl font-semibold mb-3">Location</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          <div>
+            <p>Property Location</p>
+            <input
+              type="text"
+              placeholder=""
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div>
+            <p>Zip Code</p>
+            <input
+              type="number"
+              placeholder=""
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Seller;
